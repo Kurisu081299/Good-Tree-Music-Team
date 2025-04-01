8c8f309447805330cd3384983642679ee2bb8971
developer-talaghay <developer@talaghay.com>
2025-04-01 21:49:59 +0800

    04/06/2025

--- js/pdfConfig.js
+++ js/pdfConfig.js
@@ -1,4 +1,4 @@
-const pdfSrc = "https://drive.google.com/file/d/1WpbUgcgEoRh75zfWPyXlSOTemtr0tOfU/preview";
+const pdfSrc = "https://drive.google.com/file/d/1lxonPyTRMfiz9_siRc4_3l1b08OdyfZY/preview";
 
 document.addEventListener("DOMContentLoaded", function () {
     const iframe = document.getElementById("pdfViewer");
