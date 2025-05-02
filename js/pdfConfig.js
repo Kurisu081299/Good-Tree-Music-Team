fee291c0080c06e0187caacc1eaca2ae9a8403a6
developer-talaghay <developer@talaghay.com>
2025-05-02 08:19:12 +0800

    05/04/2025

--- js/pdfConfig.js
+++ js/pdfConfig.js
@@ -1,4 +1,4 @@
-const pdfSrc = "https://drive.google.com/file/d/1YObDfgPpnH7qOkp32dPDVKTjEGhHz43U/preview";
+const pdfSrc = "https://drive.google.com/file/d/1km1PdcYW9kUo2gt5OyZJWCauq83v5QjC/preview";
 
 document.addEventListener("DOMContentLoaded", function () {
     const iframe = document.getElementById("pdfViewer");
